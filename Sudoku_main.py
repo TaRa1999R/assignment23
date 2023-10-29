3627d886973a520cce536c2a84a0dc6085db4a8b
TaRa1999R <rezaietara78@gmail.com>
2023-10-29 19:38:58 +0330

    assignment23

--- Sudoku_main.py
+++ Sudoku_main.py
@@ -14,15 +14,30 @@ class Mainwindow ( QMainWindow ) :
         super().__init__ ()
         self.ui = Ui_MainWindow ()
         self.ui.setupUi (self)
+        puzzle = Sudoku (3).difficulty (0.5)
+        self.cells = [[None for i in range (9)] for j in range (9)]
 
         for i in range (9) :
             for j in range (9) :
                 new_cell = QLineEdit ()
-                new_cell.setSizePolicy (QSizePolicy.MinimumExpanding , QSizePolicy.MinimumExpanding)
+                new_cell.setSizePolicy (QSizePolicy.Maximum , QSizePolicy.MinimumExpanding)
                 new_cell.setStyleSheet ("background-color: rgb(0,255,255); border-top-left-radius: 0px; border-top-right-radius: 0px; border-bottom-left-radius: 0px; border-bottom-right-radius: 0px;")
                 new_cell.setAlignment (Qt.AlignHCenter)
                 new_cell.setFont (QFont ("Segoe UI" , 13))
+                if puzzle.board[i][j] != None :
+                    new_cell.setText (str (puzzle.board[i][j]))
+                    new_cell.setReadOnly (True)
+
                 self.ui.grid.addWidget ( new_cell , i , j)
+                self.cells[i][j] = new_cell
+                new_cell.textChanged.connect (partial (self.validation , i , j) )
+
+    def validation ( self , i , j , text) : 
+        if text not in ["1" , "2" , "3" , "4" , "5" , "6" , "7" , "8" , "9"] :
+            self.cells[i][j].setText ("")
+
+
+    def check ( self ) : ...
 
 if __name__ == "__main__" :
     app = QApplication ( sys.argv )
